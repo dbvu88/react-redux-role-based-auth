b5fc3b77eefab327c8d96fb422e7d2c7e78df9ee
Duc Vu <de.yd315@gmail.com>
2019-06-29 06:39:26 +0700

    created user action

---
+++ src/_actions/user.actions.js
@@ -0,0 +1,76 @@
+import {
+    userConstants
+} from '../_constants'
+import {
+    userService
+} from '../_services'
+import {
+    alertActions
+} from './'
+import {
+    history
+} from '../_helpers'
+
+
+const login = (username, password) => {
+
+    const {
+        LOGIN_REQUEST,
+        LOGIN_SUCCESS,
+        LOGIN_FAILURE
+    } = userConstants
+
+    return dispatch => {
+        dispatch({
+            type: LOGIN_REQUEST,
+            user
+        })
+
+        userService.login(username, password)
+            .then(user => {
+                    dispatch({
+                        type: LOGIN_SUCCESS,
+                        user
+                    })
+                    history.push('/')
+                },
+                error => {
+                    dispatch({
+                        type: LOGIN_FAILURE,
+                        error
+                    })
+                    dispatch(alertActions.error(error))
+                }
+            )
+    }
+}
+
+const getAll = () => {
+
+    const {
+        GETALL_REQUEST,
+        GETALL_SUCCESS,
+        GETALL_FAILURE
+    } = userConstants
+
+    return dispatch => {
+        dispatch({
+            type: GETALL_REQUEST
+        })
+
+        userService.getAll().then(
+            users => dispatch(users),
+            error => {
+                dispatch({
+                    type: LOGIN_FAILURE,
+                    error
+                })
+            }
+        )
+    }
+}
+export const userActions = {
+    login,
+    logout,
+    getAll
+}
